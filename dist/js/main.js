d7bf257e493dea47f0ab2fc9607a4a149d86bae6
Stephen Podlogar <stpodlogar@gmail.com>
2021-11-23 20:58:07 -0500

    Changes: - places call corrections - extract lat lng from request

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,54 +1,45 @@
+let autocomplete;
+function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'), 
+        {
+            types: ['(cities)'],
+            fields: ['geometry', 'name']
+        });
+    autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged() {
+    const place = autocomplete.getPlace();
+
+    if (!place.geometry) {
+        document.getElementById('autocomplete').placeholder = 'Enter a place...';
+    } else {
+        const latitude = place.geometry.location.lat();
+        const longitude = place.geometry.location.lng();
+        getWeather([latitude, longitude]);
+    }
+}
 
 document.querySelector('button').addEventListener('click', () => {
     let city = document.querySelector('#autocomplete');
     getWeather(city.value)
   });
   
-const getWeather = async (city) => {
+const getWeather = async (coords) => {
     const urlDataObj = {
-        city: city
+        latitude: coords[0],
+        longitude: coords[1]
     };
     try {
         const response = await fetch("./.netlify/functions/get_weather", {
             method: "POST",
             body: JSON.stringify(urlDataObj)
         });
-        // const dataStream = await fetch("./.netlify/serverless/get_weather")
         const data = await response.json();
         console.log(data);
     } catch (err) {
         console.log(err);
     };
 }
-
-// const searchElement = document.querySelector('[data-city-search]');
-// const searchBox = new google.maps.places.SearchBox(searchElement);
-// searchElement.addListener('places_changed', () => {
-//     const place = searchBox.getPlaces()[0];
-//     if (place === null) return;
-//     const latitude = place.geometry.location.lat();
-//     const longitude = place.geometry.location.lng();
-//     console.log(latitude, longitude);
-// })
-
-// let autocomplete;
-// function initAutocomplete() {
-//     autocomplete = new google.maps.places.Autocomplete(
-//         document.getElementById('autocomplete'), 
-//         {
-//             types: ['cities'],
-//             fields: ['place_id', 'geometry', 'name']
-//         });
-//     autocomplete.addListener('place_changed', onPlaceChanged);
-// }
-
-// function onPlaceChanged() {
-//     const place = autocomplete.getPlace();
-
-//     if (!place.geometry) {
-//         document.getElementById('autocomplete').placeholder = 'Enter a place...';
-//     } else {
-//         console.log('This happened')
-//     }
-// }
 
--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -4,9 +4,8 @@ const { API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
     const params = JSON.parse(event.body);
-    const { city } = params;
-    console.log(API_KEY);
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const { latitude, longitude } = params;
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={minutely}&appid=${API_KEY}`;
     // const encodedUrl = encodeURI(url);
     try {
         const response = await fetch(url);
